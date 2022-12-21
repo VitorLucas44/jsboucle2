b8a363d6c41f63468e3c694eec8b5ab8fdc7039d
VitorLucas44 <vitoriosolucas@hotmail.com>
2022-12-21 14:56:09 +0100

    finis

---
+++ public/js/main.js
@@ -0,0 +1,82 @@
+// exo1
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0) {
+//       continue;
+//     }
+//     console.log(i);
+//   }
+
+
+
+
+
+// exo2
+// let nombre;
+// do {
+// nombre = prompt("Saisissez un nombre pair");
+// nombre = Number(nombre);
+// } while (nombre % 2 !== 0);
+// console.log(`Vous avez saisi le nombre pair ${nombre}`);
+
+
+
+
+// exo3
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 7) {
+//     break;
+//     }
+// console.log(i);
+// }
+
+
+// exo4
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 !== 0) {
+//       continue;
+//     }
+//     console.log(i);
+//   }
+
+
+
+// exo5
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 !== 0) {
+//     continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+// exo6
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i === 5) {
+//     console.log("La boucle est fiinis");
+//     break;
+//     }
+// }
+
+
+
+// exo7
+// for (var i = 1; i <= 10; i++) {
+//     if (i % 3 === 0) {
+//     console.log("Itération a été sauté");
+//     continue;
+//     }
+//     console.log(i);
+// }
+
+
+// exo8
+for (var i = 1; i <= 10; i++) {
+    if (i === 5) {
+    console.log("La boucle estf finis");
+    break;
+    }
+    console.log(i);
+  }
